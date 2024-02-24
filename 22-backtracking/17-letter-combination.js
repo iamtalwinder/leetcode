5794b08399299f3bccc7cd7691d44a8741a8c60f
Talwinder Singh <singhtalwinder790@gmail.com>
2024-02-24 19:03:02 +0530

    fix: letter combination

--- 22-backtracking/17-letter-combination.js
+++ 22-backtracking/17-letter-combination.js
@@ -4,34 +4,33 @@
  */
 var letterCombinations = function (digits) {
   if (!digits) {
-      return []
+    return []
   }
-  
+
   const phoneMap = {
-      2: ['a', 'b', 'c'],
-      3: ['d', 'e', 'f'],
-      4: ['g', 'h', 'i'],
-      5: ['j', 'k', 'l'],
-      6: ['m', 'n', 'o'],
-      7: ['p', 'q', 'r', 's'],
-      8: ['t', 'u', 'v'],
-      9: ['w', 'x', 'y', 'z']
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z']
   }
 
   const result = [];
 
   function backtrack(path, index) {
-      if (path.length === digits.length) {
-          result.push(path);
-          return;
-      }
+    if (path.length === digits.length) {
+      result.push(path);
+      return;
+    }
 
+    const numberChars = phoneMap[digits[index]];
 
-      const numberChars = phoneMap[digits[index]];
-
-      numberChars.forEach(char => {
-          backtrack(path + char, index + 1);
-      });
+    numberChars.forEach(char => {
+      backtrack(path + char, index + 1);
+    });
 
   }
 
