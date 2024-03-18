18597bf0e7831ea2234bd12e5c2a66bf54b58aff
Talwinder Singh <singhtalwinder790@gmail.com>
2024-03-18 20:13:17 +0530

    feat: add reverse linked list

---
+++ 6-in-place-reversal-of-a-linked-list/206-reverse-linked-lists.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  let prev = null, current = head;
+
+
+  while (current) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+};
