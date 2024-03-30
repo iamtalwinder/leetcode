efd3140a2b2e938a1e75498a008aaa19eeaf7c72
Talwinder Singh <singhtalwinder790@gmail.com>
2024-03-30 13:15:00 +0530

    feat: add leetcode 199

---
+++ 10-tree-breadth-first-search/199-binary-tree-right-side-view.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  const queue = [root], result = [];
+
+  while (queue.length) {
+    const levelLength = queue.length;
+
+    for (let i = 1; i <= levelLength; i++) {
+      const node = queue.shift()
+      if (i === levelLength) {
+        result.push(node.val);
+      }
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+
+  return result;
+};
